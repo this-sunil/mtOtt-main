2cbf69133500b69d85d97528d0d4a9d8e119bb5e
mtteam21 <swarajya888@gmail.com>
2023-01-25 18:33:22 +0530

    commit

--- lib/utility/theme/ThemeCubit.dart
+++ lib/utility/theme/ThemeCubit.dart
@@ -1,24 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class ThemeCubit extends Cubit<ThemeData> {
   ThemeCubit() : super(ThemeData.dark().copyWith(
       cardColor: const Color(0xFF222222),
-      appBarTheme: const AppBarTheme(
+      appBarTheme:  AppBarTheme(
+          titleTextStyle:GoogleFonts.inter(fontWeight: FontWeight.w500,fontSize: 20,color:
+          Colors.white),
 
-      backgroundColor: Color(0xFF222222))));
+      backgroundColor: const Color(0xFF222222))));
 
   void toggleTheme(){
-    if(state==ThemeData.light().copyWith(primaryColorLight: Colors.white,appBarTheme: const AppBarTheme(
+    if(state==ThemeData.light().copyWith(primaryColorLight: Colors.white,appBarTheme:  AppBarTheme(
+        titleTextStyle:GoogleFonts.inter(fontWeight: FontWeight.w600,fontSize: 20,color:
+        Colors.black),
        backgroundColor: Colors.white))){
       emit(ThemeData.dark().copyWith(
           cardColor: const Color(0xFF222222),
-          appBarTheme: const AppBarTheme(backgroundColor: Color(0xFF222222))));
+          appBarTheme:   AppBarTheme(
+              titleTextStyle:GoogleFonts.inter(fontWeight: FontWeight.w500,fontSize: 20,color:
+              Colors.white),
+              backgroundColor: const Color(0xFF222222))));
     }
     else{
       emit(ThemeData.light().copyWith(primaryColorLight: Colors.white,
           cardColor: Colors.white,
-          appBarTheme: const AppBarTheme(
+          appBarTheme:  AppBarTheme(
+            titleTextStyle:GoogleFonts.inter(fontWeight: FontWeight.w500,fontSize: 20,color:
+            Colors.black),
 
           backgroundColor: Colors.white)));
     }
